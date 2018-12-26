6f96fdf5e6b204bd744566cf48e21047e2276972
lee0821 <lee0821@yeah.net>
2018-12-26 17:45:58 +0800

    second commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div>
+      <input v-model="inputValue"/>
+      <button @click="handleClick">提交</button>
+    </div>
+    <ul>
+      <todo-item v-for="(item, index) of list" :key="index" :content="item" :index="index" @delete="itemDelete"></todo-item>  
+    </ul>
+  </div>  
+</template>
+
+<script>
+import TodoItem from "./components/TodoItem"
+
+export default {
+  components:{
+    "todo-item" : TodoItem,
+  },
+  data: function() {
+    return({
+      inputValue: '',
+      list: [],
+      }
+    )
+  },
+  methods: {
+    handleClick: function() {
+      this.list.push(this.inputValue);
+      this.inputValue = '';
+    },
+    itemDelete: function(index){
+      this.list.splice(index, 1);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
